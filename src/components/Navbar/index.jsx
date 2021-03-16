a391f03a13594ec52ff3f20fa58acd3df21eb025
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-03-16 19:28:37 +0100

    feat: add `react-router-dom` and SignUp route

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { ButtonPrimary, ButtonSecondary } from '../Buttons';
 
 import { Brand, Header, HeaderStyles, HeaderStylesRight } from './navbar.style';
 
-export const Navbar = () => (
-  <HeaderStyles>
-    <Header>
-      <Brand>RECORD STORE</Brand>
-      <HeaderStylesRight>
-        <ButtonSecondary type='button'>Login</ButtonSecondary>
-        <ButtonPrimary type='button'>Sign up</ButtonPrimary>
-      </HeaderStylesRight>
-    </Header>
-  </HeaderStyles>
-);
+export const Navbar = () => {
+  const history = useHistory();
+
+  const handleGoToSignUp = () => history.push('/signup');
+
+  return (
+    <HeaderStyles>
+      <Header>
+        <Brand>RECORD STORE</Brand>
+        <HeaderStylesRight>
+          <ButtonSecondary type='button'>Login</ButtonSecondary>
+          <ButtonPrimary type='button' onClick={handleGoToSignUp}>
+            Sign up
+          </ButtonPrimary>
+        </HeaderStylesRight>
+      </Header>
+    </HeaderStyles>
+  );
+};
