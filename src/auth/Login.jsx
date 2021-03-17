2babf956dbb172ed18784998732b0257a9157eb8
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-03-17 14:13:55 +0100

    feat: add user-context

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -12,34 +12,46 @@ import {
 import backgroundImage from '../components/imgs/Login.png';
 import { PrimaryText, Title } from '../components/Text';
 import { GridAuth } from '../components/Grid';
+import { useInput } from '../hooks/useInput';
 
-export const Login = () => (
-  <SignUpStyles>
-    <GridAuth height='600px'>
-      <FormStlyes>
-        <div>
-          <Title>Welcome back!!</Title>
-          <PrimaryText>Please fill in your credentials</PrimaryText>
-        </div>
-        <Form>
+export const Login = () => {
+  const [emailProps, resetEmail] = useInput('');
+  const [passwordProps, resetPassword] = useInput('');
+
+  const handleSignUpSubmit = (e) => {
+    e.preventDefault();
+    resetEmail();
+    resetPassword();
+  };
+
+  return (
+    <SignUpStyles>
+      <GridAuth height='600px'>
+        <FormStlyes>
           <div>
-            <Input placeholder='Email' width='100%' />
+            <Title>Welcome back!!</Title>
+            <PrimaryText>Please fill in your credentials</PrimaryText>
           </div>
-          <div>
-            <Input placeholder='Password' width='100%' />
-          </div>
-          <ButtonTertiaryStyles>
-            <Button secondary type='button'>
-              Log in
-            </Button>
-          </ButtonTertiaryStyles>
-        </Form>
-        <p>
-          What? You don&apos;t have an account? Create one
-          <Link to='/signup'> here!</Link>
-        </p>
-      </FormStlyes>
-      <ImageSignUp background={backgroundImage} />
-    </GridAuth>
-  </SignUpStyles>
-);
+          <Form onSubmit={handleSignUpSubmit}>
+            <div>
+              <Input {...emailProps} placeholder='Email' width='100%' />
+            </div>
+            <div>
+              <Input {...passwordProps} placeholder='Password' width='100%' />
+            </div>
+            <ButtonTertiaryStyles>
+              <Button secondary type='submit'>
+                Log in
+              </Button>
+            </ButtonTertiaryStyles>
+          </Form>
+          <p>
+            What? You don&apos;t have an account? Create one
+            <Link to='/signup'> here!</Link>
+          </p>
+        </FormStlyes>
+        <ImageSignUp background={backgroundImage} />
+      </GridAuth>
+    </SignUpStyles>
+  );
+};
